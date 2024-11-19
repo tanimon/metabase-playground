653b0d9adde4c3a14bf16121cbb247465004a104
tanimon <8575113+tanimon@users.noreply.github.com>
2024-11-19 17:19:13 +0900

    Add Metabase App Runner Service

--- iac/lib/main-stack.ts
+++ iac/lib/main-stack.ts
@@ -1,16 +1,25 @@
+import * as apprunner from '@aws-cdk/aws-apprunner-alpha';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MainStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new apprunner.Service(this, 'MetabaseService', {
+      source: apprunner.Source.fromAsset({
+        asset: new cdk.aws_ecr_assets.DockerImageAsset(
+          this,
+          'MetabaseDockerImage',
+          {
+            directory: './asset/business-intelligence',
+            platform: cdk.aws_ecr_assets.Platform.LINUX_AMD64,
+          }
+        ),
+        imageConfiguration: {
+          port: 3000,
+        },
+      }),
+    });
   }
 }
